2f0e2c21b6fe4009c3afb3e86182e4ba1e32e30a
jorge5a <jorge@ciclopesl.com>
2021-12-30 11:26:18 +0100

    Mi test django

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jorge/Documentos/proyectos/becarios/djangotest/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
